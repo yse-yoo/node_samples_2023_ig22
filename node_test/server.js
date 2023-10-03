3720f2010451669583485801907cef40349a016f
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-10-03 11:09:35 +0900

    update server

---
+++ node_test/server.js
@@ -0,0 +1,13 @@
+// expressモジュール読み込み
+const express = require('express')
+// dotenvモジュール読み込み
+const dotenv = require('dotenv')
+
+//設定読み込み
+dotenv.config()
+const HOST = process.env.HOST
+const PORT = process.env.PORT
+
+console.log(HOST)
+console.log(PORT)
+
