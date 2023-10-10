2cb9bd3c1ab7cf5938244b9b7b4a5879ddf57c9d
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-10-10 11:12:39 +0900

    update

--- node_test/server.js
+++ node_test/server.js
@@ -3,12 +3,13 @@
 const express = require('express')
 // dotenvモジュール読み込み
 const dotenv = require('dotenv')
+// routesモジュール読み込み
+const routes = require('.routes')
 
 //設定読み込み
 dotenv.config()
 const HOST = process.env.HOST
 const PORT = process.env.PORT
-
 
 //Webサーバ作成
 const app = express()
@@ -21,6 +22,8 @@ app.use(express.static(__dirname + '/public'))
 // URLエンコード（マルチバイト文字の対策）
 app.use(express.urlencoded({ extended: true }))
 
+// Routerの利用
+app.use(routes)
 
 
 // サーバー待機（たいき） wait 
