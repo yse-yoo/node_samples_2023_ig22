bcd31b99b4d66841ba78bea5e6c3d60b732d4a68
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-10-24 10:33:10 +0900

    update

--- node_test/controllers/HomeController.js
+++ node_test/controllers/HomeController.js
@@ -1,10 +1,6 @@
 
 // トップページの処理
 exports.index = (req, res) => {
-    console.log(req.body)
-    console.log(req.url)
-    console.log(req.query)
-    // res.send('トップページ')
     //views/index.ejsを表示
     res.render('index')
 }
