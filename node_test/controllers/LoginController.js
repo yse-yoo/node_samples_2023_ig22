766d23a7e4760a0e370c79a14c48c4c452024e17
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-10-31 11:03:49 +0900

    add auth

--- node_test/controllers/LoginController.js
+++ node_test/controllers/LoginController.js
@@ -1,3 +1,4 @@
+// models/user.js モジュールを読み込む
 const user = require('../models/user')
 
 // ログイントップ
@@ -8,19 +9,25 @@ exports.index =  (req, res) => {
 
 // 認証処理
 exports.auth = (req, res) => {
-    var loginName = req.body.login_name
+    var email = req.body.login_name
     var password = req.body.password
 
-    var message = "ログイン失敗..."
-    var authUser = user.auth(loginName, password);
+    var authUser = user.auth(email, password);
     console.log(authUser)
+    var message = "ログイン失敗..."
+    if (authUser) {
+        message = "ログイン成功！"
+    }
+
+    // var loginName = req.body.login_name
+    // var password = req.body.password
+    // var message = "ログイン失敗..."
     // if (loginName == process.env.LOGIN_NAME
     //     && password == process.env.PASSWORD) {
     //     message = "ログイン成功！"
     // }
     // console.log(loginName)
     // console.log(password)
-
 
     res.send(message)
 }
