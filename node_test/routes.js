e865e6f96943dc88d85f050344ecb618a8ebc15b
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-10-31 09:32:45 +0900

    update

--- node_test/routes.js
+++ node_test/routes.js
@@ -1,7 +1,5 @@
 // Expressモジュール読み込み
 const express = require('express')
-// models/itemモジュール読み込み
-const item = require('./models/item')
 
 // Routerの利用
 const router = express.Router()
@@ -9,6 +7,7 @@ const router = express.Router()
 // controllersモジュール読み込み
 const HomeController = require('./controllers/HomeController')
 const LoginController = require('./controllers/LoginController')
+const ItemController = require('./controllers/ItemController')
 
 // HomeController
 router.get('/', HomeController.index)
@@ -18,27 +17,8 @@ router.get('/profile', HomeController.profile)
 router.get('/login', LoginController.index)
 router.post('/auth', LoginController.auth)
 
-// 商品一覧（しょうひんいちらん）
-router.get('/item/', (req, res) => {
-    //データをすべて取得
-    var data = {
-        items: item.get()
-    }
-    // views/item/index.ejs に dataを渡して表示
-    res.render('item/index', data)
-})
-
-
-// 商品詳細（しょうひんしょうさい）
-router.get('/item/:id', (req, res) => {
-    const id = req.params.id
-    // データを検索して用意
-    var data = {
-        item: item.find(id)
-    }
-    // views/item/detail.ejs に dataを渡して表示
-    res.render('item/detail', data)
-})
-
+// ItemController
+router.get('/item/', ItemController.index)
+router.get('/item/:id', ItemController.detail)
 
 module.exports = router
