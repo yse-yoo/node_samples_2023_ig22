e2659b79c675c564d187a35aacc481c1a2d6ad21
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-11-14 11:08:30 +0900

    update

--- node_test/test_dbconnect.js
+++ node_test/test_dbconnect.js
@@ -2,6 +2,7 @@ const mysql = require('mysql2');
 const dotenv = require('dotenv')
 dotenv.config()
 
+// mysqlの接続設定
 const db_info = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
@@ -10,3 +11,13 @@ const db_info = {
     password: process.env.DB_PASS,
 }
 console.log("Database Info", db_info)
+
+//DB接続
+const con = mysql.createConnection(db_info)
+con.connect((error) => {
+    //エラーの場合
+    if (error) throw error;
+    //成功の場合
+    console.log("DB Connect!!!");
+})
+con.end();
