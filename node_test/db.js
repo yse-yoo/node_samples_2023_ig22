59d6761797815daf199cebded6c17b9263d3f673
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-11-14 11:38:15 +0900

    add db.js

---
+++ node_test/db.js
@@ -0,0 +1,10 @@
+const dotenv = require('dotenv')
+dotenv.config()
+
+exports.info = {
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    port: process.env.DB_PORT,
+    password: process.env.DB_PASS,
+}
